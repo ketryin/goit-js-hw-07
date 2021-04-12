a8f74372b0a08efb368aaae7992ddb24c3474774
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-04-12 21:36:27 +0300

    fixed event name

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector('#name-input');
 const userName = document.querySelector('#name-output');
 
-inputEl.addEventListener('keydown', (event) => {
+inputEl.addEventListener('input', (event) => {
     userName.textContent = event.target.value === ''
         ? 'незнакомец'
         : event.target.value;
